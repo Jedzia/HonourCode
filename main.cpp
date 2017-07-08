e1ac7ad05702c7798b889cded378a60ab979fb46
Jedzia <jed69@gmx.de>
2017-07-08 20:30:03 +0200

    ic(initial check in)

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "hello honour" << endl;
+    system("PAUSE"); // JFUCKEd UP yo are windowze ====??????
+    //later we will show you independence
+    return EXIT_SUCCESS; / Really
+}
