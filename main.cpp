f3399f86c5e1bd0769d23446d0402bfd7e372030
Jedzia <jed69@gmx.de>
2023-11-04 08:35:30 +0100

    missing comment

--- main.cpp
+++ main.cpp
@@ -8,5 +8,5 @@ int main(int argc, char *argv[])
     cout << "hello honour" << endl;
     system("PAUSE"); // JFUCKEd UP yo are windowze ====??????
     //later we will show you independence
-    return EXIT_SUCCESS; / Really
+    return EXIT_SUCCESS; // Really
 }
